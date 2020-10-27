cad6062b27bf3ae96f2d3ed16d3f6fe7438ae15a
svulpala <sowmyavulpala@yahoo.com>
2020-10-27 14:49:46 -0500

    Assigns 1st commit

---
+++ HelloWorld_Git/src/Helloworld_Git/HelloWorldGit.java
@@ -0,0 +1,10 @@
+package Helloworld_Git;
+
+public class HelloWorldGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Helloworld");
+	}
+
+}
